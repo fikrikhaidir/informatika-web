8c2bcacf05d7d261528b4282d2ae5e11355cb6dc
fikrikhaidir <fikrikhaidir@gmail.com>
2017-04-02 02:41:20 +0700

    working with pages

--- landing/urls.py
+++ landing/urls.py
@@ -4,6 +4,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^profil/$', views.profil, name='profil'),
+    url(r'^kurikulum/$', views.kurikulum, name='kurikulum'),
+    url(r'^ukm/$', views.ukm, name='ukm'),
+    url(r'^alumni/$', views.alumni, name='alumni'),
+
     # url(r'^berita/$', views.listBerita, name='berita'),
     # url(r'^adm/detail_berita/(?P<id>\d+)/$', views.detail_berita, name='detail_berita'),
     # url(r'^timeline/$', views.view_timeline, name='timeline'),
--- landing/views.py
+++ landing/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
 def home(request):
-    
+
     return render(request,"home.html")
+
+def kurikulum(request):
+    return render(request,"kurikulum.html")
+
+def profil(request):
+    return render(request,"profil.html")
+
+def ukm(request):
+    return render(request,"ukm.html")
+
+def alumni(request):
+    return render(request,"alumni.html")
