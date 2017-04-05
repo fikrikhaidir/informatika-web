6fb3c93c304c6bd9b1294fc1801cd391a00f8b40
AchmadKurnianto <achmad18kurnianto@gmail.com>
2017-04-05 11:09:25 +0700

    percobaan integrasi slack bitbucket

--- landing/models.py
+++ landing/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class alumni(models.Model):
+	nama = models.CharField(default='', max_length=200)
+	alamat = models.CharField(default="",max_length=300)
+
